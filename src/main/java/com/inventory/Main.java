1764455dbda82366d9a27b2a6b786d2ff26e1d63
NotSoGaurav <gauravtiwari11feb@gmail.com>
2026-09-18 16:18:42 +0530

    Initial structure

---
+++ src/main/java/com/inventory/Main.java
@@ -0,0 +1,6 @@
+package com.inventory;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/inventory/exception/InsufficientStockException.java
@@ -0,0 +1,4 @@
+package com.inventory.exception;
+
+public class InsufficientStockException extends Exception {
+}
